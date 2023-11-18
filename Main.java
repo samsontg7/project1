592dbd13be23f122e0587220272c2c5f80b35ecf
samsontg7 <147622859+samsontg7@users.noreply.github.com>
2023-11-18 14:57:30 +0300

    Update Main.java
    I have updated my code

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Classroom classroom = new Classroom("Math");
-        Student student1 = new Student("John", 18, true);
-        Student student2 = new Student("Alice", 17, false);
+        Student student1 = new Student("Abel", 20, true);
+        Student student2 = new Student("Nahom", 15, false);
 
         classroom.addStudent(student1);
         classroom.addStudent(student2);
@@ -15,8 +15,8 @@ public class Main {
             System.out.println(student.getName());
         }
 
-        Book book = new Book("hello there", "F. mustefa");
-        Person person = new Person("Jselam", 20, false);
+        Book book = new Book("hello there", " fraol");
+        Person person = new Person("mahlet", 20, false);
 
         Rental rental = new Rental(book, person);
         System.out.println(rental.getBook().getTitle());
